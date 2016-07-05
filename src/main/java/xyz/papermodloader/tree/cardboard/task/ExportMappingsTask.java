503ac6e8d32bfe91f631aee59a87ec988f329011
iLexiconn <me@ilexiconn.net>
2016-07-05 17:19:31 +0200

    Update cardboard tasks

--- src/main/java/xyz/papermodloader/tree/cardboard/task/ExportMappingsTask.java
+++ src/main/java/xyz/papermodloader/tree/cardboard/task/ExportMappingsTask.java
@@ -2,14 +2,32 @@ package xyz.papermodloader.tree.cardboard.task;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
-import xyz.papermodloader.book.converter.MappingsConverter;
+import org.gradle.internal.logging.progress.ProgressLoggerFactory;
+import xyz.papermodloader.book.converter.exporter.Exporters;
+import xyz.papermodloader.book.util.ProgressLogger;
 import xyz.papermodloader.tree.cardboard.CardboardConstants;
 
 import java.io.IOException;
 
-public class ExportMappingsTask extends DefaultTask {
+public class ExportMappingsTask extends DefaultTask implements ProgressLogger {
+    public org.gradle.internal.logging.progress.ProgressLogger progressLogger;
+
     @TaskAction
     public void doTask() throws IOException {
-        MappingsConverter.main(new String[] {"--input", CardboardConstants.ENIGMA_MAPPINGS.get().getAbsolutePath(), "--output", CardboardConstants.BOOK_MAPPINGS.get().getAbsolutePath()});
+        if (CardboardConstants.BOOK_MAPPINGS.get().exists()) {
+            String yn = System.console().readLine("Are you sure you want to overwrite " + CardboardConstants.BOOK_MAPPINGS.get().getName() + "? (Y/N) ");
+            if (!yn.equals("Y")) {
+                return;
+            }
+        }
+        this.progressLogger = this.getServices().get(ProgressLoggerFactory.class).newOperation(this.getClass());
+        this.progressLogger.start(":exportMappings", ":export");
+        Exporters.getExporter("json").export(CardboardConstants.ENIGMA_MAPPINGS.get(), CardboardConstants.BOOK_MAPPINGS.get(), this);
+        this.progressLogger.completed();
+    }
+
+    @Override
+    public void onProgress(int current, int total) {
+        this.progressLogger.progress(current + "/" + total + " (" + (int) ((current / (float) total) * 100) + "%)");
     }
 }
--- src/main/java/xyz/papermodloader/tree/cardboard/task/ImportMappingsTask.java
+++ src/main/java/xyz/papermodloader/tree/cardboard/task/ImportMappingsTask.java
@@ -2,10 +2,32 @@ package xyz.papermodloader.tree.cardboard.task;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.internal.logging.progress.ProgressLoggerFactory;
+import xyz.papermodloader.book.converter.exporter.Exporters;
+import xyz.papermodloader.book.util.ProgressLogger;
+import xyz.papermodloader.tree.cardboard.CardboardConstants;
 
-public class ImportMappingsTask extends DefaultTask {
+import java.io.IOException;
+
+public class ImportMappingsTask extends DefaultTask implements ProgressLogger {
+    public org.gradle.internal.logging.progress.ProgressLogger progressLogger;
+
     @TaskAction
-    public void doTask() {
-        throw new RuntimeException("This function is not yet functional!");
+    public void doTask() throws IOException {
+        if (CardboardConstants.ENIGMA_MAPPINGS.get().exists()) {
+            String yn = System.console().readLine("Are you sure you want to overwrite " + CardboardConstants.ENIGMA_MAPPINGS.get().getName() + "? (Y/N) ");
+            if (!yn.equals("Y")) {
+                return;
+            }
+        }
+        this.progressLogger = this.getServices().get(ProgressLoggerFactory.class).newOperation(this.getClass());
+        this.progressLogger.start(":importMappings", ":import");
+        Exporters.getExporter("enigma").export(CardboardConstants.BOOK_MAPPINGS.get(), CardboardConstants.ENIGMA_MAPPINGS.get(), this);
+        this.progressLogger.completed();
+    }
+
+    @Override
+    public void onProgress(int current, int total) {
+        this.progressLogger.progress(current + "/" + total + " (" + (int) ((current / (float) total) * 100) + "%)");
     }
 }
